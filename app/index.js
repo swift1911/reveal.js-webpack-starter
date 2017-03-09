e6145aed0f2cfbb0ae290302ec0bcce873490c92
duyalin <duyalin@fangdd.com>
2017-03-09 16:30:55 +0800

    first ppt

--- app/index.js
+++ app/index.js
@@ -1,23 +1,36 @@
-(function(){
-  "use strict";
+(function () {
+    "use strict";
 
-  // include index.html for webpack to load
-  require("./index.html");
+    // include index.html for webpack to load
+    require("./index.html");
 
-  var Reveal = require("reveal.js");
-  require("reveal.js/css/reveal.css");
-  // uncomment theme you want to load below
-  require("reveal.js/css/theme/night.css");
+    var Reveal = require("reveal.js");
+    require("reveal.js/css/reveal.css");
+    // uncomment theme you want to load below
+    require("reveal.js/css/theme/night.css");
 
+    require('reveal.js/lib/js/head.min');
 
-  Reveal.initialize({
-    history: true,
-    // More info https://github.com/hakimel/reveal.js#dependencies
-   /* dependencies: [
-      { src: 'plugin/markdown/marked.js' },
-      { src: 'plugin/markdown/markdown.js' },
-      { src: 'plugin/notes/notes.js', async: true },
-      { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
-    ]*/
-  });
+    window.addEventListener("mousedown", handleClick, false);
+    window.addEventListener("contextmenu", function(e) { e.preventDefault(); }, false);
+
+    function handleClick(e) {
+        e.preventDefault();
+        if(e.button === 0) Reveal.next();
+        if(e.button === 2) Reveal.prev();
+    }
+
+    Reveal.initialize({
+        // Parallax background image
+        parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+        parallaxBackgroundSize: '2100px 900px',
+
+        parallaxBackgroundHorizontal: 200,
+        parallaxBackgroundVertical: 50,
+        history: true,
+        // More info https://github.com/hakimel/reveal.js#dependencies
+        dependencies: [
+             { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        ]
+    });
 })();
